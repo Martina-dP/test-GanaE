2e19c7b66b2b04cadb97f896f93583a9fbdcc217
Martina del Pino <martudelpino@gmail.com>
2023-01-17 09:29:34 -0300

    Inicio

---
+++ back/src/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const { MONGO_USER, MONGO_PASSWORD} = process.env
+
+const MONGO_URI = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.kzrpuvp.mongodb.net/test`;
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+.then(() => console.log('DB connected'))
+.catch(err => console.log(err));
